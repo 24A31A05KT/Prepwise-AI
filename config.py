b16595d2cda77adc25561a9c83f94ee06819a7a6
Jagan Raju <jagannadharajulokareddi@gmail.com>
2026-06-27 02:18:20 +0530

    Fix config syntax

--- config.py
+++ config.py
@@ -3,25 +3,8 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads", "resumes")
-
-    SECRET_KEY = "prepwise_secret_key"
-
-    SQLALCHEMY_DATABASE_URI = \
-        "sqlite:///" + os.path.join(
-            BASE_DIR,
-            "database",
-            "app.db"
-        )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = Falseimport os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "prepwise_secret_key")
 
-    # Create folders automatically
     DATABASE_DIR = os.path.join(BASE_DIR, "database")
     os.makedirs(DATABASE_DIR, exist_ok=True)
 
